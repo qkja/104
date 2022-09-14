ae8eb2543617d1e919f1f9f471c6a4b1dd187cae
algorithmic-jacko <qkj1205@163.com>
2022-09-14 19:45:11 +0800

    数据结构剑指offer 第六天

--- 2022/test_09_12/RBTree.h
+++ 2022/test_09_12/RBTree.h
@@ -1,7 +1,11 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #pragma once
 #include <iostream>
+#include <queue>
 using std::pair;
+using std::cout;
+using std::endl;
+using std::queue;
 namespace bit
 {
 	enum Colour
@@ -20,9 +24,9 @@ namespace bit
 			,_col(RED)
 		{}
 
-		RBTreeNode<K, V> _left;
-		RBTreeNode<K, V> _right;
-		RBTreeNode<K, V> _parent;
+		RBTreeNode<K, V>* _left;
+		RBTreeNode<K, V>* _right;
+		RBTreeNode<K, V>* _parent;
 		pair<K, V> _kv;
 		Colour _col;
 	};
@@ -72,7 +76,7 @@ namespace bit
 			node->_col = RED;           // 记住 不要相信构造函数,有可能不是你写的,不会自动默认0
 
 			// 插入元素
-			if (data->first > parent->_kv->first)
+			if (data.first > parent->_kv.first)
 			{
 				parent->_right = node;
 			}
@@ -99,7 +103,7 @@ namespace bit
 					{
 						// 变色
 						parent->_col = BLACK;
-						uncle->_col = BALCK;
+						uncle->_col = BLACK;
 						grandfather->_col = RED;
 						cur = grandfather;
 						parent = cur->_parent;
@@ -112,7 +116,7 @@ namespace bit
 						    //   p
 						    // c
 							// 右旋
-							RotateR(grandfater);
+							RotateR(grandfather);
 							// 变色
 							parent->_col = BLACK;
 							grandfather->_col = RED;
@@ -124,7 +128,7 @@ namespace bit
 							//    c
 							// 左旋 +  右旋
 							RotateL(parent);
-							RotateR(grandfater);
+							RotateR(grandfather);
 							cur->_col = BLACK;
 							grandfather->_col = RED;
 						}
@@ -164,7 +168,7 @@ namespace bit
 							//   c
 							// 右旋 +  左旋
 							RotateR(parent);
-							RotateL(grandfater);
+							RotateL(grandfather);
 							cur->_col = BLACK;
 							grandfather->_col = RED;
 						}
@@ -177,16 +181,188 @@ namespace bit
 			return true;
 		}
 
+		void inorder()
+		{
+			_inorder(_pRoot);
+		}
+		void levelOrder()
+		{
+			_levelOrder();
+		}
+		int maxHeight()
+		{
+			return _maxHeight(_pRoot);
+		}
+		int minHeight()
+		{
+			return _minHeight(_pRoot);
+		}
+
+		bool IsValidRBTree()
+		{
+			Node* pRoot = _pRoot;
+			// 空树也是红黑树
+			if (nullptr == pRoot)
+				return true;
+			// 检测根节点是否满足情况
+			if (BLACK != pRoot->_col)
+			{
+				cout << "违反红黑树性质二：根节点必须为黑色" << endl;
+				return false;
+			}
+
+			// 获取任意一条路径中黑色节点的个数
+			size_t blackCount = 0;
+			Node* pCur = pRoot;
+			while (pCur)
+			{
+				if (BLACK == pCur->_col)
+					blackCount++;
+				pCur = pCur->_left;
+			}
+			// 检测是否满足红黑树的性质，k用来记录路径中黑色节点的个数
+			size_t k = 0;
+			return _IsValidRBTree(pRoot, k, blackCount);
+		}
+
+
 	private:
+
+
+		bool _IsValidRBTree(Node* pRoot, size_t k, const size_t blackCount)
+		{
+			//走到null之后，判断k和black是否相等
+			if (nullptr == pRoot)
+			{
+				if (k != blackCount)
+				{
+					cout << "违反性质四：每条路径中黑色节点的个数必须相同" << endl;
+					return false;
+				}
+				return true;
+			}
+			// 统计黑色节点的个数
+			if (BLACK == pRoot->_col)
+				k++;
+			// 检测当前节点与其双亲是否都为红色 遇到 红色节点 检查 父亲
+			Node* pParent = pRoot->_parent;
+			if (pParent && RED == pParent->_col && RED == pRoot->_col)
+			{
+				cout << "违反性质三：没有连在一起的红色节点" << endl;
+				return false;
+			}
+			return _IsValidRBTree(pRoot->_left, k, blackCount) &&
+				_IsValidRBTree(pRoot->_right, k, blackCount);
+		}
+
+
+		int _maxHeight(Node* root)
+		{
+			if (root == nullptr)
+				return 0;
+			int left = _maxHeight(root->_left);
+			int right = _maxHeight(root->_right);
+			return left > right ?  left + 1 : right + 1;
+		}
+		int _minHeight(Node* root)
+		{
+			if (root == nullptr)
+				return 0;
+			int left = _minHeight(root->_left);
+			int right = _minHeight(root->_right);
+			return left < right ?  left + 1 : right + 1;
+		}
+		void _levelOrder()
+		{
+			if (_pRoot == nullptr)
+				return;
+
+			// 层序遍历
+			//queue<int*> q;
+			queue<Node*> q;
+			q.push(_pRoot);
+			int size = 1;
+			while (size != 0)
+			{
+				Node* top = q.front();
+				if (top->_left)
+					q.push(top->_left);
+				if (top->_right)
+					q.push(top->_right);
+				q.pop();
+				--size;
+				cout << top->_kv.first << " ";
+				if (size == 0)
+				{
+					cout << endl;
+					size = q.size();
+				}
+			}
+		}
+		void _inorder(Node* root)
+		{
+			if (root == nullptr)
+				return;
+			_inorder(root->_left);
+			cout << root->_kv.first << " ";
+			_inorder(root->_right);
+		}
 		// 旋转
 		void RotateL(Node* parent)
 		{
-
+			Node* sub = parent->_right;
+			Node* subR = sub->_left;
+			parent->_right = subR;
+			if (subR)
+				subR->_parent = parent;
+			Node* prev = parent->_parent;
+			
+			sub->_left = parent;
+			parent->_parent = sub;
+			if (prev == nullptr)
+			{
+				_pRoot = sub;
+				_pRoot->_parent = nullptr;
+			}
+			else
+			{
+				if (prev->_left == parent)
+					prev->_left = sub;
+				else
+					prev->_right = sub;
+			
+				sub->_parent = prev;
+			}
+	
 		}
 		void RotateR(Node* parent)
 		{
-
+			Node* subL = parent->_left;
+			Node* subLR = subL->_right;
+			parent->_left = subLR;
+			if (subLR)
+				subLR->_parent = parent;
+			Node* prev = parent->_parent;
+			
+			parent->_parent = subL;
+			subL->_right = parent;
+					
+			//pParent 有可能不是 根
+			if (prev == nullptr)
+			{
+				_pRoot = subL;
+				_pRoot->_parent = nullptr;
+				return;
+			}
+			
+			if (prev->_left == parent)
+				prev->_left = subL;
+			else
+				prev->_right = subL;
+					
+			subL->_parent = prev;
 		}
+		
 	private:
 		Node* _pRoot = nullptr;
 	};
--- 2022/test_09_12/test.cpp
+++ 2022/test_09_12/test.cpp
@@ -1,1 +1,84 @@
 #define _CRT_SECURE_NO_WARNINGS 1
+#include <iostream>
+#include <time.h>
+#include "RBTree.h"
+using namespace std;
+
+//int main()
+//{
+//	int N = 1024;
+//
+//	bit::RBTree<int, int> rb;
+//	for (int i = 0; i < N; i++)
+//	{
+//		rb.Insert(make_pair(i, i));
+//	}
+//	cout << "ÊÇ·ñÆ½ºâ " << rb.IsValidRBTree() << endl;
+//	return 0;
+//}
+
+
+
+
+
+
+//int main()
+//{
+//	int N = 1024;
+//
+//	bit::RBTree<int, int> rb;
+//	for (int i = 0; i < N; i++)
+//	{
+//		rb.Insert(make_pair(i, i));
+//	}
+//	cout << rb.maxHeight() << endl;
+//	cout << rb.minHeight() << endl;
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	int N = 1024;
+//
+//	bit::RBTree<int, int> rb;
+//	for (int i = 0; i < N; i++)
+//	{
+//		rb.Insert(make_pair(i, i));
+//	}
+//	cout << rb.maxHeight() << endl;
+//	cout << rb.minHeight() << endl;
+//	//rb.inorder();
+//	//rb.levelOrder();
+//	return 0;
+//}
+
+//int main()
+//{
+//	int N = 10;
+//	srand(time(0));
+//
+//	bit::RBTree<int, int> rb;
+//	for (int i = 0; i < N; i++)
+//	{
+//		int ret = rand();
+//		rb.Insert(make_pair(ret, ret));
+//	}
+//
+//	rb.inorder();
+//	//rb.levelOrder();
+//	return 0;
+//}
+
+//int main()
+//{
+//	bit::RBTree<int, int> rb;
+//	for (int i = 0; i < 10; i++)
+//	{
+//		rb.Insert(make_pair(i, i));
+//	}
+//
+//	rb.inorder();
+//	//rb.levelOrder();
+//	return 0;
+//}

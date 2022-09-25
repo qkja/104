aed794cc9a1b886a8375cdfba98ca269f0f74046
algorithmic-jacko <qkj1205@163.com>
2022-09-25 15:47:21 +0800

    再析指针进阶面试题

--- 2022/test_09_24/test.c
+++ 2022/test_09_24/test.c
@@ -1,2 +1,26 @@
 #define _CRT_SECURE_NO_WARNINGS 1
-//问题  二维数组传参
+//问题  指针问题
+#include <stdio.h>
+
+int main()
+{
+	int a[] = { 1,2,3,4 };
+	printf("%d\n", sizeof(a));
+	printf("%d\n", sizeof(a + 0));
+	printf("%d\n", sizeof(*a));
+	printf("%d\n", sizeof(a + 1));
+	printf("%d\n", sizeof(a[1]));
+	printf("%d\n", sizeof(&a));
+	printf("%d\n", sizeof(*&a));
+	printf("%d\n", sizeof(&a + 1));
+	printf("%d\n", sizeof(&a[0]));
+	printf("%d\n", sizeof(&a[0] + 1));
+
+	//字符数组
+	/*char arr[] = { 'a','b','c','d','e','f' };
+	printf("%d\n", sizeof(arr));
+	printf("%d\n", sizeof(arr + 0));
+	printf("%d\n", sizeof(*arr));
+	printf("%d\n", sizeof(arr[1]));*/
+	return 0;
+}

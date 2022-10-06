1ec4f539b25bb843cfa8a8296b6932d449f63097
algorithmic-jacko <qkj1205@163.com>
2022-10-06 21:50:41 +0800

    每日一天 day 1,2复习结束

--- 2022/test_10_06/main.cpp
+++ 2022/test_10_06/main.cpp
@@ -65,145 +65,221 @@ using namespace std;
 //	cout << "实际概率 : " << (x * 1.0) / N << endl;
 //	return 0;
 //}
-#include <stdlib.h>
-#include <Windows.h>
+//#include <stdlib.h>
+//#include <Windows.h>
+//
+//class Exception
+//{
+//public:
+//	Exception(const string& errmsg, int id)
+//		:_errmsg(errmsg)
+//		, _id(id)
+//	{}
+//	// 虚函数
+//	virtual string what() const
+//	{
+//		return _errmsg;
+//	}
+//	// 这里 
+//	int getId() const
+//	{
+//		return _id;
+//	}
+//protected:
+//	string _errmsg;
+//	int _id; // 错误码
+//};
+//// 数据库异常
+//class SqlException : public Exception
+//{
+//public:
+//	SqlException(const string& errmsg, int id, const string& sql)
+//		:Exception(errmsg, id) // 帮助父类构造
+//		, _sql(sql) // sql语句
+//	{}
+//	virtual string what() const
+//	{
+//		string str = "SqlException:";
+//		str += _errmsg;
+//		str += "->";
+//		str += _sql;
+//		return str;
+//	}
+//private:
+//	const string _sql;
+//};
+//
+//// 缓存异常
+//class CacheException : public Exception
+//{
+//public:
+//	CacheException(const string& errmsg, int id)
+//		:Exception(errmsg, id)
+//	{}
+//	virtual string what() const
+//	{
+//		string str = "CacheException:";
+//		str += _errmsg;
+//		return str;
+//	}
+//};
+//
+//// 网络异常
+//class HttpServerException : public Exception
+//{
+//public:
+//	HttpServerException(const string& errmsg, int id, const string& type)
+//		:Exception(errmsg, id)
+//		, _type(type)
+//	{}
+//	virtual string what() const
+//	{
+//		string str = "HttpServerException:";
+//		str += _type;
+//		str += ":";
+//		str += _errmsg;
+//		return str;
+//	}
+//private:
+//	const string _type;
+//};
+//
+//void SQLMgr()
+//{
+//	srand(time(0));
+//	if (rand() % 7 == 0)
+//	{
+//		throw SqlException("权限不足", 100, "select * from name = '张三'");
+//	}
+//	//cout << "数据库 正确" << endl;
+//	//throw "xxxxxx";
+//}
+//void SeedMsg(const string& str)
+//{
+//	if (rand() < RAND_MAX - 20000)
+//	{
+//		throw HttpServerException("SeedMsg::网络错误", 2, "put");
+//	}
+//	else if (rand() < RAND_MAX / 2)
+//	{
+//		throw HttpServerException("SeedMsg::你已经不是对方好友", 1, "post");
+//	}
+//	else
+//	{
+//		cout << "消息发送成功！->" << str << endl;
+//	}
+//}
+//void CacheMgr()
+//{
+//	srand(time(0));
+//	if (rand() % 5 == 0)
+//	{
+//		throw CacheException("权限不足", 100);
+//	}
+//	else if (rand() % 6 == 0)
+//	{
+//		throw CacheException("数据不存在", 101);
+//	}
+//	SQLMgr();
+//}
+//void HttpServer()
+//{
+//	// ...
+//	srand(time(0));
+//	// 0 1 2  66%的概率
+//	if (rand() % 3 == 0)
+//	{
+//		throw HttpServerException("请求资源不存在", 100, "get");
+//	}
+//	 
+//	else if (rand() % 4 == 0)
+//	{
+//		throw HttpServerException("权限不足", 101, "post");
+//	}
+//
+//	// 这个   网络请求正确
+//	CacheMgr();
+//}
+//
+//int main()
+//{
+//	while (1)
+//	{
+//		//this_thread::sleep_for(chrono::seconds(1));
+//		::Sleep(1000);
+//
+//		for (size_t i = 0; i < 10; ++i)
+//		{
+//			try
+//			{
+//				SeedMsg("你好啊！今晚一起看电影怎么样？");
+//			}
+//			catch (const Exception& e)
+//			{
+//				if (e.getId() == 2) // 异常编码的价值，针对某个错误进行特殊处理
+//				{
+//					cout << "网络错误，重试发消息第" << i << "次" << endl;
+//					continue;
+//				}
+//				else // 其他错误
+//				{
+//					cout << e.what() << endl;
+//
+//					break;
+//					//throw e;	// 异常重新抛出 
+//				}
+//			}
+//			break;
+//
+//		}
+//
+//	
+//	}
+//	return 0;
+//}
 
-class Exception
-{
-public:
-	Exception(const string& errmsg, int id)
-		:_errmsg(errmsg)
-		, _id(id)
-	{}
-	// 虚函数
-	virtual string what() const
-	{
-		return _errmsg;
-	}
-	// 这里 
-	int getId()
-	{
-		return _id;
-	}
-protected:
-	string _errmsg;
-	int _id; // 错误码
-};
-// 数据库异常
-class SqlException : public Exception
-{
-public:
-	SqlException(const string& errmsg, int id, const string& sql)
-		:Exception(errmsg, id) // 帮助父类构造
-		, _sql(sql) // sql语句
-	{}
-	virtual string what() const
-	{
-		string str = "SqlException:";
-		str += _errmsg;
-		str += "->";
-		str += _sql;
-		return str;
-	}
-private:
-	const string _sql;
-};
+//int main()
+//{
+//	while (1)
+//	{
+//		//this_thread::sleep_for(chrono::seconds(1));
+//		::Sleep(1000);
+//		try
+//		{
+//			for (size_t i = 0; i < 10; ++i)
+//			{
+//				try
+//				{
+//					SeedMsg("你好啊！今晚一起看电影怎么样？");
+//				}
+//				catch (const Exception& e)
+//				{
+//					if (e.getId() == 2) // 异常编码的价值，针对某个错误进行特殊处理
+//					{
+//						cout << "网络错误，重试发消息第" << i << "次" << endl;
+//						continue;
+//					}
+//					else // 其他错误
+//					{
+//						//break;
+//						throw e;	// 异常重新抛出 
+//					}
+//				}
+//				break;
+//
+//			}
+//		}
+//		catch (const Exception& e) // 这里捕获父类对象就可以
+//		{
+//			// 多态
+//			cout << e.what() << endl;
+//		}
+//		catch (...)
+//		{
+//			cout << "Unkown Exception" << endl;
+//		}
+//
+//	}
+//	return 0;
+//}
 
-// 缓存异常
-class CacheException : public Exception
-{
-public:
-	CacheException(const string& errmsg, int id)
-		:Exception(errmsg, id)
-	{}
-	virtual string what() const
-	{
-		string str = "CacheException:";
-		str += _errmsg;
-		return str;
-	}
-};
-
-// 网络异常
-class HttpServerException : public Exception
-{
-public:
-	HttpServerException(const string& errmsg, int id, const string& type)
-		:Exception(errmsg, id)
-		, _type(type)
-	{}
-	virtual string what() const
-	{
-		string str = "HttpServerException:";
-		str += _type;
-		str += ":";
-		str += _errmsg;
-		return str;
-	}
-private:
-	const string _type;
-};
-
-void SQLMgr()
-{
-	srand(time(0));
-	if (rand() % 7 == 0)
-	{
-		throw SqlException("权限不足", 100, "select * from name = '张三'");
-	}
-	//cout << "数据库 正确" << endl;
-	//throw "xxxxxx";
-}
-void CacheMgr()
-{
-	srand(time(0));
-	if (rand() % 5 == 0)
-	{
-		throw CacheException("权限不足", 100);
-	}
-	else if (rand() % 6 == 0)
-	{
-		throw CacheException("数据不存在", 101);
-	}
-	SQLMgr();
-}
-void HttpServer()
-{
-	// ...
-	srand(time(0));
-	// 0 1 2  66%的概率
-	if (rand() % 3 == 0)
-	{
-		throw HttpServerException("请求资源不存在", 100, "get");
-	}
-	 
-	else if (rand() % 4 == 0)
-	{
-		throw HttpServerException("权限不足", 101, "post");
-	}
-
-	// 这个   网络请求正确
-	CacheMgr();
-}
-int main()
-{
-	while (1)
-	{
-		//this_thread::sleep_for(chrono::seconds(1));
-		::Sleep(1000);
-		try {
-			HttpServer();
-		}
-
-		catch (const Exception& e) // 这里捕获父类对象就可以
-		{
-			// 多态
-			cout << e.what() << endl;
-		}
-		catch (...)
-		{
-			cout << "Unkown Exception" << endl;
-		}
-	}
-	return 0;
-}
--- 2022/test_10_06/test.cpp
+++ 2022/test_10_06/test.cpp
@@ -1,8 +1,130 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include <iostream>
 #include <vector>
+#include <algorithm>
 #include <string>
+#include <unordered_set>
 using namespace std;
+
+
+//day 2
+// 常量指针 const int* p ;
+// 指针常量 int* onst p ;
+//排序子序列
+
+int main()
+{
+	int n = 0;
+	cin >> n;
+	vector<int> v;
+	v.reserve(n + 1);
+
+	int ret = 0;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> ret;
+		v.push_back(ret);
+	}
+	// 这个的作用  避免越界
+	// 也避免 1 2 3 2 2 这种情况
+	v[n] = 0;
+
+	// 计数器 
+	int count = 0;
+	int i = 0; // 这个是一个循环
+	while (i < n)
+	{
+		// 非递减序列
+		if (v[i] < v[i + 1])
+		{
+			// 主意不能一直加 范围 i+1也会越界
+			while (i < n && v[i] <= v[i + 1])
+			{
+				i++;
+			}
+			count++;
+			i++;
+		}
+		else if (v[i] == v[i + 1])
+		{
+			i++;
+		}
+		else
+		{
+			while (i < n && v[i] >= v[i + 1])
+			{
+				i++;
+			}
+			count++;
+			i++;
+		}
+	}
+	cout << count << endl;
+	return 0;
+}
+
+////////////////////////////////////////////////////////
+// day 1
+// 循环输入
+//int main()
+//{
+//	int n = 0;
+//	while (cin >> n)
+//	{
+//		cout << n << endl;;
+//	}
+//	return 0;
+//}
+
+// 组队竞赛
+//int main()
+//{
+//	int n = 0;
+//	cin >> n;
+//	if (n <= 0)
+//		return 0;
+//	vector<int> v;
+//	v.reserve(3 * n);
+//	int ret = 0;
+//	for (int i = 0; i < 3 * n; i++)
+//	{
+//		cin >> ret;
+//		v.push_back(ret);
+//	}
+//	sort(v.begin(), v.end());
+//	int sum = 0;
+//	for (int i = 0; i < n; i++)
+//	{
+//		sum += v[3 * n - 2 * (i + 1)];
+//	}
+//	cout << sum;
+//	return 0;
+//}
+// 删除公共字符串 这里用 hash
+//int main() 
+//{
+//	string str1;
+//	string str2;
+//	getline(std::cin, str1);
+//	getline(std::cin, str2);
+//	unordered_set<char> s;
+//	// 遍历 str2 放在 set
+//	s.insert(str2.begin(), str2.end());
+//	// 遍历 str1 在str2对应的  set中找不到
+//	string str;
+//
+//	for (auto ch : str1)
+//	{
+//		if (s.find(ch) == s.end())
+//		{
+//			str += ch;
+//		}
+//	}
+//	cout << str;
+//	return 0;
+//}
+
+
 //struct Goods
 //{
 //	string _name; // 名字
@@ -20,4 +142,3 @@ using namespace std;
 //	v.resize(2);
 //	return 0;
 //}
-

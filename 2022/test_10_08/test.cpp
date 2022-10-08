2404b3a28e24abca04c2fb87d9d8c4e91f64d0e1
algorithmic-jacko <qkj1205@163.com>
2022-10-08 16:26:09 +0800

    笔试强训 day 7,8复习结束

---
+++ 2022/test_10_08/test.cpp
@@ -0,0 +1,151 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <iostream>
+#include <string>
+#include <stack>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+// day 8
+// 引用的 底层是由指针实现的,本质就是一个指针 
+// 求最小公倍数 //这里找个简单的方法
+// 最小公倍数 = 乘积 / 最大公约数
+// 最大公约数 辗转相除法
+// 9 6 
+// 9%6 = 3
+// 6%3 = 0
+// 3就是最大公约数
+//int main()
+//{
+//	int x = 0;
+//	int y = 0;
+//	cin >> x >> y;
+//	int mul = x * y;
+//	int c = 0;
+//	while (c = x % y)
+//	{
+//		x = y;
+//		y = c;
+//	}
+//	// 这里  y 就是最大公约数
+//	cout << mul / y << endl;
+//	return 0;
+//}
+//WY20 两种排序方法
+//bool sortByLength(const vector<string>& v)
+//{
+//	for (int i = 1; i < v.size(); i++)
+//	{
+//		if (v[i].size() < v[i - 1].size())
+//			return false;
+//	}
+//	return true;
+//}
+//bool lexSort(const vector<string>& v)
+//{
+//	for (int i = 1; i < v.size(); i++)
+//	{
+//		if (v[i] < v[i - 1])
+//			return false;
+//	}
+//	return true;
+//}
+//int main()
+//{
+//	int n = 0;
+//	cin >> n;
+//	vector<string> v;
+//	v.resize(n);
+//	for (int i = 0; i < n; i++)
+//	{
+//		cin >> v[i];
+//	}
+//	if (lexSort(v) && sortByLength(v))
+//		cout << "both" << endl;
+//	else if (lexSort(v))
+//		cout << "lexicographically" << endl;
+//
+//	else if(sortByLength(v))
+//		cout << "lengths" << endl;
+//	else 
+//		cout << "none" << endl;
+//
+//	
+//	return 0;
+//}
+
+/////////////////////////////////////////////
+// day 7
+// 初始化列表那里才是 初始化 
+// 函数体内是赋值
+// 初始化列表 是管不住的 static 的  所有对象共享
+// 必须放在 初始化列表的 的变量
+// const
+// 引用
+// 没有默认构造函数的 自定义对象   ---> 这里建议 有的也放进入 效率高点
+//合法括号序列判断
+//class Parenthesis {
+//public:
+//    bool chkParenthesis(string A, int n) {
+//        // write code here
+//        if (A.empty())
+//            return true;
+//        if (A.size() != n)
+//            return false;
+//        stack<char> s;
+//        for (int i = 0; i < n; i++)
+//        {
+//            if (A[i] == '(')
+//            {
+//                // 入栈
+//                s.push(A[i]);
+//            }
+//            else if (A[i] == ')')
+//            {
+//                // 出栈
+//                if (s.empty())
+//                    return false;
+//                s.pop();
+//            }
+//            else
+//            {
+//                return false;
+//            }
+//        }
+//        return s.empty();
+//    }
+//};
+//int Fib(int n)
+//{
+//	
+//	int left = 0;
+//	int right = 1;
+//	int sum = 1;
+//	while (sum <= n)
+//	{
+//		sum = left + right;
+//		left = right;
+//		right = sum;
+//	}
+//	return sum;
+//}
+//int main()
+//{
+//	int n = 0;
+//	cin >> n;
+//	// 寻找第一个 不比 n 小的 数
+//	int left = 0;
+//	int right = 1;
+//	int sum = 1;
+//	// n = 2
+//	while (sum <= n)
+//	{
+//		sum = left + right; // 1 2
+//		left = right;       // 1 1
+//		right = sum;        // 1 2
+//	}
+//
+//	int ret = min(sum - n, n - left);
+//	cout << ret << endl;
+//	return 0;
+//}

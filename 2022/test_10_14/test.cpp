415d2348a2dc105b8db66af9b76f76b2a494111c
algorithmic-jacko <qkj1205@163.com>
2022-10-14 22:32:01 +0800

    笔试强训 day 22

---
+++ 2022/test_10_14/test.cpp
@@ -0,0 +1,55 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <iostream>
+#include <string>
+#include <map>
+
+//using namespace std;
+//int main()
+//{
+//	string str;
+//	getline(cin, str);
+//	map<char, int> m;
+//	for (char ch : str)
+//	{
+//		m[ch]++;
+//	}
+//	int flag = 0;
+//	for (char ch : str)
+//	{
+//		if (m[ch] == 1)
+//		{
+//			cout << ch << endl;
+//		    flag = 1;
+//			break;
+//		}
+//	}
+//	if (flag == 0)
+//		cout << -1 << endl;
+//	return 0;
+//}
+
+//int main()
+//{
+//	string str;
+//	getline(cin, str);
+//	map<char, int> m;
+//	for (char ch : str)
+//	{
+//		m[ch]++;
+//	}
+//	int flag = 0;
+//	for (auto p:m)
+//	{
+//		if (p.second == 1)
+//		{
+//			cout << p.first << endl;
+//			flag = 1;
+//			break;
+//		}
+//	}
+//	if (flag == 0)
+//	{
+//		cout << -1 << endl;
+//	}
+//	return 0;
+//}

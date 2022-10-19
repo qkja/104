9c9ab3491cbf6fce5a565641dffc32dd51c5a5ca
algorithmic-jacko <qkj1205@163.com>
2022-10-19 23:13:39 +0800

    非阻塞等

---
+++ 2022/test_10_19/SeqList.h
@@ -0,0 +1,55 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#ifndef __SEQLIST_H__                                                                                                                                                                                                                                                                                      
+#define __SEQLIST_H__
+
+#include <stdio.h>
+#include <assert.h>
+#include <stdlib.h>
+
+#endif
+
+
+// 好修改数据的 类型
+typedef int SLDateType;
+
+typedef struct SeqList
+{
+	SLDateType* array;
+	size_t size;
+	size_t capacity;
+} SeqList;
+
+//void SeqListInit(SeqList ps);
+void SeqListInit(SeqList* ps);
+
+
+// 对数据的管理:增删查改 
+// 问题  初始化为何使用 指针传参 简单的值拷贝
+// 尤其是里面的指针  这也是简单的值拷贝
+//void SeqListInit(SeqList* ps);
+//
+//// 这里可以用结构体传参吗
+//void SeqListDestroy(SeqList* ps);
+//
+//// 指针消耗小
+void SeqListPrint(SeqList* ps);
+
+void SeqListPushBack(SeqList* ps, SLDateType x);
+//
+//void SeqListPushFront(SeqList* ps, SLDateType x);
+//
+//void SeqListPopFront(SeqList* ps);
+//
+void SeqListPopBack(SeqList* ps);
+//
+//// 顺序表查找
+//int SeqListFind(SeqList* ps, SLDateType x);
+//// 顺序表在pos位置插入x
+//void SeqListInsert(SeqList* ps, size_t pos, SLDateType x);
+//// 顺序表删除pos位置的值
+//void SeqListErase(SeqList* ps, size_t pos);
+//
+//// 这里是一些辅助函数,一般不爆露接口
+//// 检测容量
+//// 不够直接扩容 够什么都不做
+//void SeqListCheckCapacity(SeqList* ps);
---
+++ 2022/test_10_19/test.c
@@ -0,0 +1,72 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include "SeqList.h"                                                                                                                                                                                                                                                                                                                                                   
+//int main()
+//{
+//	SeqList s;
+//	SeqListInit(&s);
+//	return 0;
+//}
+//void f(int b)
+//{
+//	b = 20;
+//}
+//int main()
+//{
+//	int a = 0;
+//	f(a);
+//	printf("%d", a);
+//	return 0;
+//}
+//void TestInit()
+//{
+//
+//	SeqList s;
+//	SeqListInit(&s);
+//}
+
+void TestPuskBack()
+{
+
+	SeqList s;
+	SeqListInit(&s);
+	SeqListPushBack(&s, 1);
+	SeqListPushBack(&s, 2);
+	SeqListPrint(&s);
+}
+
+void TestPopBack()
+{
+	SeqList s;
+	SeqListInit(&s);
+
+	SeqListPushBack(&s, 1);
+	SeqListPushBack(&s, 2);
+	SeqListPushBack(&s, 3);
+	SeqListPushBack(&s, 4);
+
+	SeqListPrint(&s);
+	SeqListPopBack(&s);
+	printf("===============\n");
+
+	SeqListPrint(&s);
+}
+
+int main()
+{
+	TestPopBack();
+	return 0;
+}
+
+//int main()
+//{
+//	int arr[10] = { 0 };
+//	arr[11] = 1;
+//	printf("%d", arr[11]);
+//	return 0;
+//}
+
+//int main()
+//{
+//	TestPuskBack();
+//	return 0;
+//}

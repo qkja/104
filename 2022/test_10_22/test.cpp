c11ae5e775e25f6d9f968de9f94db73ceb868c0e
algorithmic-jacko <qkj1205@163.com>
2022-10-22 17:39:36 +0800

    笔试强训day29

--- 2022/test_10_22/test.cpp
+++ 2022/test_10_22/test.cpp
@@ -1,6 +1,79 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 
+#include <iostream>
+#include <vector>
+using namespace std;
+//class Solution {
+//public:
+//	// 写一个尝试
+//	// 解释一下 在pos位置及其以后找到几个数何为 sum
+//	bool IsExist(vector<int> arr, int pos, int sum)
+//	{
+//		if (pos == arr.size() && sum != 0)
+//			return false;
+//		if (sum == 0)
+//			return true;
+//
+//		// 这里没有找到
+//		//开始吧
+//		bool b1 = false;
+//		bool b2 = false;
+//		if(arr[pos] <= sum)
+//			b1 = IsExist(arr, pos + 1, sum - arr[pos]); // 这里要
+//		b2 = IsExist(arr, pos + 1, sum);               // 这里不要
+//
+//		return b1 || b2;
+//	}
+//	int getFirstUnFormedNum(vector<int> arr, int len) {
+//		if (arr.empty())
+//			return -1;
+//		int min = arr[0];
+//		int max = arr[0];
+//		for (int i = 1; i < len; i++)
+//		{
+//			max += arr[i];
+//			if (arr[i] < min)
+//				min = arr[i];
+//		}
+//		// 现在得到 [min, max]
+//		// 开始寻找
+//		
+//		for (int i = min + 1; i < max; i++)
+//		{
+//			if (!IsExist(arr, 0 ,i))
+//			{
+//				return i;
+//			}
+//		}
+//		return max + 1;
+//	}
+//};
 
+
+
+//int main()
+//{
+//	vector<int> v;
+//	v.push_back(3);
+//	v.push_back(2);
+//	v.push_back(1);
+//	int ret = Solution().getFirstUnFormedNum(v, v.size());
+//	cout << ret << endl;
+//
+//	return 0;
+//}
+
+
+
+
+
+
+
+//int main()
+//{
+//	//http//www.taobao.com;
+//	return 0;
+//}
 
 //////////////////////////////////////////
 // day 12

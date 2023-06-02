5131219cebd81e9788db70ba1ce5a8432b1f87ec
qkja <2660015902@qq.com>
2023-06-02 17:47:16 +0800

    day 09

---
+++ StrongTraining/day09/test.cc
@@ -0,0 +1,73 @@
+/**
+ * User: Qkj
+ * Description: 
+ * Date: 2023-06-02
+ * Time: 17:26
+ */
+//class UnusualAdd {
+//  public:
+//    int addAB(int A, int B) {
+//      // write code here
+//      if(A == 0)
+//        return  B;
+//      if(B==0)
+//        return  A;
+//      while(A){
+//        int ret = (A&B) << 1;
+//        B = A^B;
+//        A = ret;
+//      }
+//      return B;
+//    }
+//};
+//
+//
+//#include <iostream>
+//#include <string>
+//#include <stack>
+//#include <vector>
+//#include <algorithm>
+//using namespace std;
+//
+//int f(int a, int b)
+//{
+//  if (a == 0 || b == 0)
+//    return 1;
+//  return f(a - 1, b) + f(a, b - 1);
+//}
+//
+//int main() {
+//  int a, b;
+//  cin >> a >> b;
+//  cout << f(a, b) << endl;
+//}
+
+
+
+
+
+//class A
+//{
+//  //const static int a = 10;
+//  public:
+//  A()
+//{
+//  a = 10;
+//}
+//
+//private:
+//~A(){}
+//  int a;
+//};
+//
+//int main(){
+//  A* p = new A;
+//  return 0;
+//}
+
+
+
+
+
+
+

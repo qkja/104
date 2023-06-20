0d9abee320bcab17e89417150c2584a7166241b9
qkja <2660015902@qq.com>
2023-06-20 22:41:39 +0800

    滑动窗口 1/2

--- OptimizationAlgorithmCourse/lesson02/test.cc
+++ OptimizationAlgorithmCourse/lesson02/test.cc
@@ -6,7 +6,9 @@
  */
 #include <vector>
 #include <iostream>
+#include <unordered_set>
 #include <algorithm>
+#include <unordered_map>
 using namespace std;
 // https://leetcode.cn/problems/minimum-size-subarray-sum/submissions/
 //  class Solution
@@ -33,10 +35,84 @@ using namespace std;
 //   }
 // };
 // https://leetcode.cn/problems/longest-substring-without-repeating-characters/submissions/
+// class Solution
+// {
+// public:
+//   bool isRepeat(int left, int right, string &str)
+//   {
+//     if (left > right)
+//       return false;
+//     unordered_set<char> s;
+//     for (int i = left; i <= right; i++)
+//     {
+//       s.insert(str[i]);
+//     }
+//     return (right - left + 1) == (int)s.size();
+//   }
+
+//   int lengthOfLongestSubstring(string s)
+//   {
+//     if (s.empty())
+//       return 0;
+//     int begin = 0;
+//     int maxLen = 0;
+//     int end = 0;
+//     for (size_t end = 0; end < s.size(); end++)
+//     {
+//       if (isRepeat(begin, end, s))
+//       {
+//         maxLen = max(maxLen, (int)end - begin + 1);
+//       }
+//       else
+//       {
+//         while (!isRepeat(begin, end, s))
+//         {
+//           begin++;
+//         }
+//       }
+//     }
+//     // maxLen = max(maxLen, (int)end - begin + 1);
+
+//     return maxLen;
+//   }
+// };
+
 class Solution
 {
 public:
   int lengthOfLongestSubstring(string s)
   {
+    if (s.empty())
+      return 0;
+    int begin = 0;
+    int maxLen = 0;
+    int end = 0;
+    unordered_map<char, int> m;
+    for (size_t end = 0; end < s.size(); end++)
+    {
+      // 判断是不是在hash 表呢
+      auto iter = m.find(s[end]);
+      // 判断是不是在hash 表中
+      if (iter == m.end())
+      {
+        // 进窗口
+        m[s[end]] = end;
+        maxLen = max(maxLen, (int)end - begin + 1);
+      }
+      else
+      {
+        while (iter != m.end()) // 找了这个元素
+        {
+          m.erase(s[begin++]);
+          iter = m.find(s[end]);
+        }
+        m[s[end]] = end;
+      }
+
+      // 更新结果
+    }
+    // maxLen = max(maxLen, (int)end - begin + 1);
+
+    return maxLen;
   }
 };

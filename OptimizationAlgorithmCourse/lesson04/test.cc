a2272af22d0b0a7f21a439cec038be7d6ea9ce80
qkja <2660015902@qq.com>
2023-06-26 09:38:38 +0800

    前缀和  1/4

---
+++ OptimizationAlgorithmCourse/lesson04/test.cc
@@ -0,0 +1,101 @@
+/**
+ * User: Qkj
+ * Description:
+ * Date: 2023-06-26
+ * Time: 08:13
+ */
+#include <iostream>
+#include <vector>
+using namespace std;
+// https://www.nowcoder.com/practice/acead2f4c28c401889915da98ecdc6bf?tpId=230&tqId=2021480&ru=/exam/oj&qru=/ta/dynamic-programming/question-ranking&sourceUrl=%2Fexam%2Foj%3Fpage%3D1%26tab%3D%25E7%25AE%2597%25E6%25B3%2595%25E7%25AF%2587%26topicId%3D196
+// int main()
+// {
+//   int n = 0;
+//   int q = 0;
+
+//   while (cin >> n >> q)
+//   {
+//     vector<int> v(n, 0);
+//     for (int i = 0; i < n; i++)
+//       cin >> v[i];
+//     for (int i = 0; i < q; i++)
+//     {
+//       int l = 0;
+//       int r = 0;
+//       cin >> l >> r;
+//       l--;
+//       r--;
+//       long long sum = 0;
+//       for (; l <= r && r < n; l++)
+//         sum += (long long)v[l];
+//       cout << sum << endl;
+//     }
+//   }
+//   return 0;
+// }
+// 64 位输出请用 printf("%lld")
+// 解法二,把这个数组从[i,j]位置开始和保存在一个二维数组中,此时
+// 可以根据   1-3 是  0-3的值减去 0-1的值来判断,这里的时间负度 n2
+// 这里可以使用一个优化,就像是在背包哪里的优化,二位数组改成一位的,不过我们每一次都要收集结果
+// 不过这里你发现没有,此时我们已经拿到答案了,这里就是前缀和
+// int main()
+// {
+//   int n = 0;
+//   int q = 0;
+
+//   while (cin >> n >> q)
+//   {
+//     vector<int> v(n + 1, 0);
+//     vector<long long> dp(n + 1, 0);
+//     for (int i = 1; i <= n; i++)
+//     {
+//       cin >> v[i];
+//       dp[i] = dp[i - 1] + (long long)v[i];
+//     }
+
+//     for (int i = 0; i < q; i++)
+//     {
+//       int l = 0;
+//       int r = 0;
+//       cin >> l >> r;
+
+//       cout << dp[r] - dp[l - 1] << endl;
+//     }
+//   }
+//   return 0;
+// }
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int n = 0;
+  int m = 0;
+  int q = 0;
+  while (cin >> n >> m >> q)
+  {
+    vector<vector<int>> v(n + 1, vector<int>(m + 1, 0));
+    vector<vector<long long>> dp(n + 1, vector<long long>(m + 1, 0));
+    for (int i = 1; i <= n; i++)
+    {
+      for (int j = 1; j <= m; j++)
+      {
+        cin >> v[i][j];
+        dp[i][j] = dp[i - 1][j] + dp[i][j - 1] + v[i][j] - dp[i - 1][j - 1];
+      }
+    }
+    for (int i = 0; i < q; i++)
+    {
+      int x1 = 0;
+      int x2 = 0;
+      int y1 = 0;
+      int y2 = 0;
+      cin >> x1 >> y1 >> x2 >> y2;
+      cout << dp[x2][y2] - dp[x1 - 1][y2] - dp[x2][y1 - 1] + dp[x1 - 1][y1 - 1] << endl;
+    }
+  }
+
+  return 0;
+}
+// 64 位输出请用 printf("%lld")

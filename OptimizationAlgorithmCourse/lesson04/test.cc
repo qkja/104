cd1980696884ba9afaf9a18a803affb3a21b822e
qkja <2660015902@qq.com>
2023-06-27 12:33:21 +0800

    前缀和  2/4

--- OptimizationAlgorithmCourse/lesson04/test.cc
+++ OptimizationAlgorithmCourse/lesson04/test.cc
@@ -65,37 +65,76 @@ using namespace std;
 //   return 0;
 // }
 
-#include <iostream>
-using namespace std;
+// #include <iostream>
+// using namespace std;
 
-int main()
-{
-  int n = 0;
-  int m = 0;
-  int q = 0;
-  while (cin >> n >> m >> q)
-  {
-    vector<vector<int>> v(n + 1, vector<int>(m + 1, 0));
-    vector<vector<long long>> dp(n + 1, vector<long long>(m + 1, 0));
-    for (int i = 1; i <= n; i++)
-    {
-      for (int j = 1; j <= m; j++)
-      {
-        cin >> v[i][j];
-        dp[i][j] = dp[i - 1][j] + dp[i][j - 1] + v[i][j] - dp[i - 1][j - 1];
-      }
-    }
-    for (int i = 0; i < q; i++)
-    {
-      int x1 = 0;
-      int x2 = 0;
-      int y1 = 0;
-      int y2 = 0;
-      cin >> x1 >> y1 >> x2 >> y2;
-      cout << dp[x2][y2] - dp[x1 - 1][y2] - dp[x2][y1 - 1] + dp[x1 - 1][y1 - 1] << endl;
-    }
-  }
+// int main()
+// {
+//   int n = 0;
+//   int m = 0;
+//   int q = 0;
+//   while (cin >> n >> m >> q)
+//   {
+//     vector<vector<int>> v(n + 1, vector<int>(m + 1, 0));
+//     vector<vector<long long>> dp(n + 1, vector<long long>(m + 1, 0));
+//     for (int i = 1; i <= n; i++)
+//     {
+//       for (int j = 1; j <= m; j++)
+//       {
+//         cin >> v[i][j];
+//         dp[i][j] = dp[i - 1][j] + dp[i][j - 1] + v[i][j] - dp[i - 1][j - 1];
+//       }
+//     }
+//     for (int i = 0; i < q; i++)
+//     {
+//       int x1 = 0;
+//       int x2 = 0;
+//       int y1 = 0;
+//       int y2 = 0;
+//       cin >> x1 >> y1 >> x2 >> y2;
+//       cout << dp[x2][y2] - dp[x1 - 1][y2] - dp[x2][y1 - 1] + dp[x1 - 1][y1 - 1] << endl;
+//     }
+//   }
 
-  return 0;
-}
+//   return 0;
+// }
 // 64 位输出请用 printf("%lld")
+// https://leetcode.cn/problems/find-pivot-index/
+// class Solution
+// {
+// public:
+//   int pivotIndex(vector<int> &nums)
+//   {
+//     nums.insert(nums.begin(), 0);
+//     vector<int> dp(nums.size(), 0);
+//     for (int i = 1; i < nums.size(); i++)
+//       dp[i] = dp[i - 1] + nums[i];
+//     for (int i = 1; i < nums.size(); i++)
+//     {
+//       if (dp[i] + dp[i - 1] == dp.back())
+//         return i - 1;
+//     }
+//     return -1;
+//   }
+// };
+// https://leetcode.cn/problems/product-of-array-except-self/
+class Solution
+{
+public:
+  // 使用前缀和的变体
+  // 前缀积
+  // 后缀积
+  vector<int> productExceptSelf(vector<int> &nums)
+  {
+    vector<int> f(nums.size(), 1);
+    vector<int> g(nums.size(), 1);
+    for (int i = 1; i < nums.size(); i++)
+      f[i] = f[i - 1] * nums[i - 1];
+    for (int i = nums.size() - 2; i >= 0; i--)
+      g[i] = g[i + 1] * nums[i + 1];
+    vector<int> ret(nums.size());
+    for (int i = 0; i < nums.size(); i++)
+      ret[i] = f[i] * g[i];
+    return ret;
+  }
+};

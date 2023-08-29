aeaafe7cf6faa8a8d01347b573de4375d6028401
qkja <qkj1205@163.com>
2023-08-29 17:12:52 +0800

    路劲问题 博客结束

--- DynamicProgramming/lesson02/test.cc
+++ DynamicProgramming/lesson02/test.cc
@@ -4,120 +4,283 @@
  * Date: 2023-05-24
  * Time: 18:58
  */
-
+#include <iostream>
 #include <vector>
 #include <algorithm>
-#include <iostream>
-
+#include <limits.h>
 using namespace std;
+class Solution
+{
+public:
+  int calculateMinimumHP(vector<vector<int>> &dungeon)
+  {
+    int m = dungeon.size(), n = dungeon[0].size();
+    // 建表 + 初始化
+    vector<vector<int>> dp(m + 1, vector<int>(n + 1, INT_MAX));
+    dp[m][n - 1] = dp[m - 1][n] = 1;
+    // 填表
+    for (int i = m - 1; i >= 0; i--)
+      for (int j = n - 1; j >= 0; j--)
+      {
+        dp[i][j] = min(dp[i + 1][j], dp[i][j + 1]) - dungeon[i][j];
+        dp[i][j] = max(1, dp[i][j]);
+      }
+    // 返回结果
+    return dp[0][0];
+  }
+};
+
+// class Solution
+// {
+// public:
+//   int minPathSum(vector<vector<int>> &grid)
+//   {
+//     int n = grid.size();
+//     int m = grid.back().size();
+//     std::vector<std::vector<int>> dp(n + 1, std::vector<int>(m + 1, INT_MAX));
+//     dp[0][1] = 0;
+//     for (size_t i = 1; i <= n; i++)
+//     {
+//       for (size_t j = 1; j <= m; j++)
+//       {
+//         dp[i][j] = std::min(dp[i][j - 1], dp[i - 1][j]) + grid[i - 1][j - 1];
+//       }
+//     }
+//     return dp[n][m];
+//   }
+// };
+
+// class Solution
+// {
+// public:
+//   int minFallingPathSum(vector<vector<int>> &matrix)
+//   {
+//     int n = matrix.size();
+//     int m = matrix.back().size();
+//     std::vector<std::vector<long long>> dp(n + 1, std::vector<long long>(m + 2,INT_MAX));
+//     for (size_t i = 0; i <= m; i++)
+//     {
+//       dp[0][i] = 0;
+//     }
+//     for (size_t i = 1; i <= n; i++)
+//     {
+//       for (size_t j = 1; j <= m; j++)
+//       {
+//         dp[i][j] = (long long)(std::min(dp[i - 1][j], std::min(dp[i - 1][j + 1], dp[i - 1][j - 1])) + (long long)matrix[i - 1][j - 1]);
+//       }
+//     }
+//     long long minVal = dp[n][0];
+//     for (size_t i = 1; i <= m; i++)
+//     {
+//       minVal = std::min(minVal, dp[n][i]);
+//     }
+//     return minVal;
+//   }
+// };
+// class Solution
+// {
+// public:
+//   int maxValue(vector<vector<int>> &grid)
+//   {
+//     int n = grid.size();
+//     int m = grid.back().size();
+//     std::vector<std::vector<int>> dp(n + 1, std::vector<int>(m + 1, 0));
+//     for (size_t i = 1; i <= n; i++)
+//     {
+//       for (size_t j = 1; j <= m; j++)
+//       {
+
+//         dp[i][j] = std::max(dp[i - 1][j], dp[i][j - 1]) + grid[i - 1][j - 1];
+//       }
+//     }
+//     return dp[n][m];
+//   }
+// };
+// class Solution
+// {
+// public:
+//   int maxValue(vector<vector<int>> &grid)
+//   {
+//     int n = grid.size();
+//     int m = grid.size();
+//     std::vector<std::vector<int>> dp(n + 1, std::vector<int>(m + 1, 0));
+//     for (size_t i = 1; i <= n; i++)
+//     {
+//       for (size_t j = 1; j <= m; j++)
+//       {
+
+//         dp[i][j] = std::max(dp[i - 1][j], dp[i][j - 1]) + grid[i - 1][j - 1];
+//       }
+//     }
+//     return dp[n][m];
+//   }
+// };
+// class Solution
+// {
+// public:
+//   int uniquePathsWithObstacles(vector<vector<int>> &obstacleGrid)
+//   {
+//     // 参数判断
+//     obstacleGrid = {{0, 0}};
+//     int n = obstacleGrid.size();
+//     int m = obstacleGrid.back().size();
+//     std::vector<std::vector<int>> dp(n + 1, std::vector<int>(m + 1, 0));
+//     std::cout << n + 1 << " " << m + 1 << std::endl;
+//     dp[0][1] = 1;
+
+//     for (size_t i = 1; i <= n; i++)
+//     {
+//       for (size_t j = 1; j <= m; j++)
+//       {
+//         if (obstacleGrid[i - 1][j - 1] == 0)
+//         {
+//           dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+//           std::cout << i << " " << j << std::endl;
+//         }
+//       }
+//     }
+//     return dp[n][m];
+//   }
+// };
+
+// int main()
+// {
+//   vector<vector<int>> obstacleGrid;
+//   Solution().uniquePathsWithObstacles(obstacleGrid);
+//   return 0;
+// }
+// class Solution
+// {
+// public:
+//   int uniquePaths(int m, int n)
+//   {
+//     // 参数检测
+//     std::vector<std::vector<int>> dp(m + 1, std::vector<int>(n + 1, 0));
+//     dp[0][1] = 1;
+//     for (size_t i = 1; i <= m; i++)
+//     {
+//       for (size_t j = 1; j <= n; j++)
+//       {
+//         dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+//       }
+//     }
+
+//     return dp[m][n];
+//   }
+// };
+// #include <vector>
+// #include <algorithm>
+// #include <iostream>
+
+// using namespace std;
 // https://leetcode.cn/problems/dungeon-game/
-class Solution
-{
-public:
-  int process1(int x, int y, int row, int col, vector<vector<int>> &grid)
-  {
-    if (x == row || y == col)
-      return INT32_MAX;
-    if (x == row - 1 && y == col - 1)
-    {
-      return std::max(1 - grid[x][y], 1);
-    }
+// class Solution
+// {
+// public:
+//   int process1(int x, int y, int row, int col, vector<vector<int>> &grid)
+//   {
+//     if (x == row || y == col)
+//       return INT32_MAX;
+//     if (x == row - 1 && y == col - 1)
+//     {
+//       return std::max(1 - grid[x][y], 1);
+//     }
 
-    int p1 = process1(x + 1, y, row, col, grid);
-    int p2 = process1(x, y + 1, row, col, grid);
+//     int p1 = process1(x + 1, y, row, col, grid);
+//     int p2 = process1(x, y + 1, row, col, grid);
 
-    return std::max(std::min(p1, p2) - grid[x][y], 1);
-  }
+//     return std::max(std::min(p1, p2) - grid[x][y], 1);
+//   }
 
-  int process2(vector<vector<int>> &grid)
-  {
-    int row = grid.size();
-    int col = grid.back().size();
-    vector<vector<int>> dp(row + 1, vector<int>(col + 1, INT32_MAX));
-    dp[row - 1][col - 1] = std::max((1 - grid[row - 1][col - 1]) % INT32_MAX, 1);
-    for (int i = row - 1; i >= 0; i--)
-    {
-      for (int j = col - 1; j >= 0; j--)
-      {
-        if (i == row - 1 && j == col - 1)
-          continue;
-        dp[i][j] = std::max((std::min(dp[i + 1][j], dp[i][j + 1]) - grid[i][j]) % INT32_MAX, 1);
-      }
-    }
-    return dp[0][0];
-  }
+//   int process2(vector<vector<int>> &grid)
+//   {
+//     int row = grid.size();
+//     int col = grid.back().size();
+//     vector<vector<int>> dp(row + 1, vector<int>(col + 1, INT32_MAX));
+//     dp[row - 1][col - 1] = std::max((1 - grid[row - 1][col - 1]) % INT32_MAX, 1);
+//     for (int i = row - 1; i >= 0; i--)
+//     {
+//       for (int j = col - 1; j >= 0; j--)
+//       {
+//         if (i == row - 1 && j == col - 1)
+//           continue;
+//         dp[i][j] = std::max((std::min(dp[i + 1][j], dp[i][j + 1]) - grid[i][j]) % INT32_MAX, 1);
+//       }
+//     }
+//     return dp[0][0];
+//   }
 
-  int calculateMinimumHP(vector<vector<int>> &grid)
-  {
-    // 假设都是负数,我们应该求的是最大的路径和
-    if (grid.empty())
-      return 0;
-    int row = grid.size();
-    int col = grid.back().size();
-    // return process1(0, 0, row, col, grid);
-    return process2(grid);
-  }
-};
-// https://leetcode.cn/problems/minimum-path-sum/submissions/
-class Solution
-{
-public:
-  int process1(int x, int y, int row, int col, vector<vector<int>> &grid)
-  {
-    if (x == row || col == y)
-      return INT32_MAX;
-    if (x == row - 1 && y == col - 1)
-      return grid[x][y];
-    int p1 = process1(x + 1, y, row, col, grid);
-    int p2 = process1(x, y + 1, row, col, grid);
-    return grid[x][y] + std::min(p1, p2);
-  }
-  int process2(vector<vector<int>> &grid)
-  {
-    int row = grid.size();
-    int col = grid.back().size();
-    vector<vector<int>> dp(row + 1, vector<int>(col + 1, INT32_MAX));
-    dp[row - 1][col - 1] = grid[row - 1][col - 1];
-    for (int i = row - 1; i >= 0; i--)
-    {
-      for (int j = col - 1; j >= 0; j--)
-      {
-        if (i == row - 1 && j == col - 1)
-          continue;
-        dp[i][j] = std::min(dp[i + 1][j], dp[i][j + 1]) + grid[i][j];
-      }
-    }
-    return dp[0][0];
-  }
-  int process3(vector<vector<int>> &grid)
-  {
-    int row = grid.size();
-    int col = grid.back().size();
-    // 一某一个位置为结尾
-    vector<vector<int>> dp(row + 1, vector<int>(col + 1, INT32_MAX));
+//   int calculateMinimumHP(vector<vector<int>> &grid)
+//   {
+//     // 假设都是负数,我们应该求的是最大的路径和
+//     if (grid.empty())
+//       return 0;
+//     int row = grid.size();
+//     int col = grid.back().size();
+//     // return process1(0, 0, row, col, grid);
+//     return process2(grid);
+//   }
+// };
+// // https://leetcode.cn/problems/minimum-path-sum/submissions/
+// class Solution
+// {
+// public:
+//   int process1(int x, int y, int row, int col, vector<vector<int>> &grid)
+//   {
+//     if (x == row || col == y)
+//       return INT32_MAX;
+//     if (x == row - 1 && y == col - 1)
+//       return grid[x][y];
+//     int p1 = process1(x + 1, y, row, col, grid);
+//     int p2 = process1(x, y + 1, row, col, grid);
+//     return grid[x][y] + std::min(p1, p2);
+//   }
+//   int process2(vector<vector<int>> &grid)
+//   {
+//     int row = grid.size();
+//     int col = grid.back().size();
+//     vector<vector<int>> dp(row + 1, vector<int>(col + 1, INT32_MAX));
+//     dp[row - 1][col - 1] = grid[row - 1][col - 1];
+//     for (int i = row - 1; i >= 0; i--)
+//     {
+//       for (int j = col - 1; j >= 0; j--)
+//       {
+//         if (i == row - 1 && j == col - 1)
+//           continue;
+//         dp[i][j] = std::min(dp[i + 1][j], dp[i][j + 1]) + grid[i][j];
+//       }
+//     }
+//     return dp[0][0];
+//   }
+//   int process3(vector<vector<int>> &grid)
+//   {
+//     int row = grid.size();
+//     int col = grid.back().size();
+//     // 一某一个位置为结尾
+//     vector<vector<int>> dp(row + 1, vector<int>(col + 1, INT32_MAX));
 
-    // 初始化
-    dp[0][1] = 0;
-    for (size_t i = 1; i <= row; i++)
-    {
-      for (size_t j = 1; j <= col; j++)
-      {
-        dp[i][j] = std::min(dp[i - 1][j], dp[i][j - 1]) + grid[i - 1][j - 1];
-      }
-    }
-    return dp[row][col];
-  }
-  int minPathSum(vector<vector<int>> &grid)
-  {
-    if (grid.empty())
-      return 0;
-    int row = grid.size();
-    int col = grid.back().size();
-    // return process1(0, 0, row, col, grid);
-    // return process2(grid);
-    return process3(grid);
-  }
-};
+//     // 初始化
+//     dp[0][1] = 0;
+//     for (size_t i = 1; i <= row; i++)
+//     {
+//       for (size_t j = 1; j <= col; j++)
+//       {
+//         dp[i][j] = std::min(dp[i - 1][j], dp[i][j - 1]) + grid[i - 1][j - 1];
+//       }
+//     }
+//     return dp[row][col];
+//   }
+//   int minPathSum(vector<vector<int>> &grid)
+//   {
+//     if (grid.empty())
+//       return 0;
+//     int row = grid.size();
+//     int col = grid.back().size();
+//     // return process1(0, 0, row, col, grid);
+//     // return process2(grid);
+//     return process3(grid);
+//   }
+// };
 // https://leetcode.cn/problems/minimum-falling-path-sum/
 
 // class Solution

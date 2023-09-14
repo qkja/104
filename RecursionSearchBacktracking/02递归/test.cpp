eac516f01b8db95da408aef67cac5761f660c584
qkja <qkj1205@163.com>
2023-09-14 15:40:43 +0800

    合并两个链表

--- RecursionSearchBacktracking/02递归/test.cpp
+++ RecursionSearchBacktracking/02递归/test.cpp
@@ -1,22 +1,48 @@
 #include <vector>
 using std::vector;
 
-class Solution {
-public:
-    void dfs(vector<int>& a, vector<int>& b, vector<int>& c, int n)
-    {
-        if (1 == n)
-        {
-            c.push_back(a.back());
-            a.pop_back();
-            return;
-        }
-        dfs(a, c, b, n - 1);
-        c.push_back(a.back());
-        a.pop_back();
-        dfs(b, a, c, n - 1);
-    }
-    void hanota(vector<int>& a, vector<int>& b, vector<int>& c) {
-        dfs(a, b, c, a.size());
-    }
-};
+//class Solution {
+//public:
+//    void dfs(vector<int>& a, vector<int>& b, vector<int>& c, int n)
+//    {
+//        if (1 == n)
+//        {
+//            c.push_back(a.back());
+//            a.pop_back();
+//            return;
+//        }
+//        dfs(a, c, b, n - 1);
+//        c.push_back(a.back());
+//        a.pop_back();
+//        dfs(b, a, c, n - 1);
+//    }
+//    void hanota(vector<int>& a, vector<int>& b, vector<int>& c) {
+//        dfs(a, b, c, a.size());
+//    }
+//};
+
+ // 1. 题目解释
+ // 2. 算法原理
+ //    2.1. 函数头
+ //    2.2. 函数体
+ // 递归出口
+//class Solution {
+//public:
+//    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+//        if (list1 == nullptr)
+//            return list2;
+//        if (nullptr == list2)
+//            return list1;
+//        if (list1->val <= list2->val)
+//        {
+//            list1->next = mergeTwoLists(list1->next, list2);
+//            return list1;
+//        }
+//        else
+//        {
+//            list2->next = mergeTwoLists(list1, list2->next);
+//            return list2;
+//        }
+//    }
+//};
+

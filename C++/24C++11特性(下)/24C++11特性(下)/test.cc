3a84a8a515c06d2f5a7c567fe30f59412423809b
qkja <qkj1205@163.com>
2023-09-30 13:24:50 +0800

    提交

--- C++/24C++11特性(下)/24C++11特性(下)/test.cc
+++ C++/24C++11特性(下)/24C++11特性(下)/test.cc
@@ -2,11 +2,20 @@
 
 #include <iostream>
 #include <functional>
+#include <string>
 
 using std::cin; 
 using std::cout;
 using std::endl;
 using std::function;
+
+//int main()
+//{
+//	std::string str = "hello";
+//	std::move(str);
+//
+//	return 0;
+//}
 //int main()
 //{
 //	//int a = 1;
@@ -53,16 +62,56 @@ using std::function;
 //	return 0;
 //}
 
+//template<class F, class T>
+//T useF(F f, T x)
+//{
+//	static int count = 0;
+//	cout << "count:" << ++count << endl;
+//	cout << "count:" << &count << endl;
+//
+//	return (f(x));
+//}
+//double f(double i) {
+//	return i / 2;
+//}
+//struct Functor
+//{
+//	double operator()(double d)
+//	{
+//		return d / 3;
+//	}
+//};
+//
+//
+//int main()
+//{
+//	// 函数名
+//	function<double(double)> fn1 = f;
+//	//function<double(double)> fn1 ( f); 支持
+//	cout << useF(fn1, 11.11) << endl;
+//
+//	// 函数对象
+//	function<double(double)> fn2 = Functor();
+//	//function<double(double)> fn2(Functor());   //不支持，因为function<double(double)>、Functor()不是同一个类型（类型匹配匹配不上）
+//	//function<double(Functor,double)> fn2=&Functor::operator();  //支持
+//	//function<double(double)> fn2 = bind(&Functor::operator(), Functor(), placeholders::_1); //支持
+//	cout << useF(fn2, 11.11) << endl;
+//
+//	// lamber表达式
+//	function<double(double)> fn3([](double d)->double { return d / 4; });
+//	cout << useF(fn3, 11.11) << endl;
+//	return 0;
+//}
 template<class F, class T>
 T useF(F f, T x)
 {
 	static int count = 0;
 	cout << "count:" << ++count << endl;
 	cout << "count:" << &count << endl;
-
-	return (f(x));
+	return f(x);
 }
-double f(double i) {
+double f(double i)
+{
 	return i / 2;
 }
 struct Functor
@@ -72,24 +121,13 @@ struct Functor
 		return d / 3;
 	}
 };
-
-
 int main()
 {
 	// 函数名
-	function<double(double)> fn1 = f;
-	//function<double(double)> fn1 ( f); 支持
-	cout << useF(fn1, 11.11) << endl;
-
+	cout << useF(f, 11.11) << endl;
 	// 函数对象
-	function<double(double)> fn2 = Functor();
-	//function<double(double)> fn2(Functor());   //不支持，因为function<double(double)>、Functor()不是同一个类型（类型匹配匹配不上）
-	//function<double(Functor,double)> fn2=&Functor::operator();  //支持
-	//function<double(double)> fn2 = bind(&Functor::operator(), Functor(), placeholders::_1); //支持
-	cout << useF(fn2, 11.11) << endl;
-
+	cout << useF(Functor(), 11.11) << endl;
 	// lamber表达式
-	function<double(double)> fn3([](double d)->double { return d / 4; });
-	cout << useF(fn3, 11.11) << endl;
+	cout << useF([](double d)->double { return d / 4; }, 11.11) << endl;
 	return 0;
 }

0352086aa8fed8634a478e8619f94c1266b85ab9
qkja <qkj1205@163.com>
2023-10-30 08:45:12 +0800

    提交

--- DataStructure/09 图/Graph.h
+++ DataStructure/09 图/Graph.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <map>
 #include <queue>
+#include <set>
 #include <vector>
 #include <string>
 #include <string.h>
@@ -15,6 +16,7 @@ namespace matrix
   class Graph
   {
     typedef Graph<V, W, MAX_W, Direction> Self;
+
   public:
     Graph() = default;
     Graph(const V *a, size_t n)
@@ -30,53 +32,53 @@ namespace matrix
 
   public:
     // 深度遍历
-    void DFS(const V& src)
+    void DFS(const V &src)
     {
       size_t srci = GetVertexIndex(src);
       std::vector<bool> visited(_vertexs.size(), false);
       _DFS(srci, visited);
     }
     // 宽度遍历
-    void BFS(const V& src) 
+    void BFS(const V &src)
     {
       size_t srci = GetVertexIndex(src);
       std::vector<bool> visited(_vertexs.size(), false);
       std::queue<size_t> q;
       q.push(srci);
       visited[srci] = true;
-      //while(!q.empty())
+      // while(!q.empty())
       //{
-      //  size_t front = q.front();
-      //  q.pop();
-      //  std::cout << front << " : " << _vertexs[front] << std::endl;
-      //  // 把他的临界顶点入队列
-      //  for(size_t i = 0; i < _matrix[front].size(); ++i)
-      //  {
-      //    if(_matrix[front][i] != MAX_W && visited[i] == false)
-      //    {
-      //      q.push(i);
-      //      visited[i] = true;
-      //    }
-      //  }
-      //}
-      
+      //   size_t front = q.front();
+      //   q.pop();
+      //   std::cout << front << " : " << _vertexs[front] << std::endl;
+      //   // 把他的临界顶点入队列
+      //   for(size_t i = 0; i < _matrix[front].size(); ++i)
+      //   {
+      //     if(_matrix[front][i] != MAX_W && visited[i] == false)
+      //     {
+      //       q.push(i);
+      //       visited[i] = true;
+      //     }
+      //   }
+      // }
+
       // 这里顺便添加一层的遍历
-      while(!q.empty())
+      while (!q.empty())
       {
         int len = q.size();
-        while(len--)
+        while (len--)
         {
           size_t front = q.front();
           q.pop();
           std::cout << front << " : " << _vertexs[front] << " ";
-          for(size_t i = 0; i < _matrix[front].size(); ++i)
+          for (size_t i = 0; i < _matrix[front].size(); ++i)
           {
-            if(_matrix[front][i] != MAX_W && visited[i] == false)
+            if (_matrix[front][i] != MAX_W && visited[i] == false)
             {
               q.push(i);
               visited[i] = true;
-            } 
-          }     
+            }
+          }
         }
         std::cout << std::endl;
       }
@@ -86,18 +88,17 @@ namespace matrix
       size_t _srci;
       size_t _dsti;
       W _w;
-      Edge(size_t srci, size_t dsti, const W& w)
-        :_srci(srci)
-         ,_dsti(dsti)
-         ,_w(w)
-      {}
-      bool operator>(const Edge& e2) const
+      Edge(size_t srci, size_t dsti, const W &w)
+          : _srci(srci), _dsti(dsti), _w(w)
+      {
+      }
+      bool operator>(const Edge &e2) const
       {
         return _w > e2._w;
       }
     };
-    // 最小生成树
-    W Kruskal(Self& minTree)
+    // 最小生成树Kruskal算法
+    W Kruskal(Self &minTree)
     {
       // 最小生成树
       minTree._vertexs = _vertexs;
@@ -105,42 +106,72 @@ namespace matrix
       minTree._matrix.resize(_vertexs.size(), std::vector<W>(_vertexs.size(), MAX_W));
       std::priority_queue<Edge, std::vector<Edge>, std::greater<Edge>> minq;
       size_t n = _vertexs.size();
-      for(size_t i = 0; i < n; ++i)
+      for (size_t i = 0; i < n; ++i)
       {
-        for(size_t j = 0; j < n; ++j)
+        for (size_t j = 0; j < n; ++j)
         {
-          if(i < j && _matrix[i][j] != MAX_W)
+          if (i < j && _matrix[i][j] != MAX_W)
           {
-            minq.push(Edge(i, j,_matrix[i][j]));
+            minq.push(Edge(i, j, _matrix[i][j]));
           }
         }
       }
       int size = 0;
       W totalW = W();
       UnionFindSet ufs(n);
-      while(!minq.empty())
+      while (!minq.empty())
       {
-         Edge min = minq.top();
-         minq.pop();
-         if(ufs.InSet(min._srci, min._dsti) == false)
-         {
-           //std::cout << _vertexs[min._srci] << "->" <<_vertexs[min._dsti] << ":" << min._w << std::endl;
-           minTree._AddEdge(min._srci, min._dsti, min._w);
-           ufs.Union(min._srci, min._dsti);
-           ++size;
-           totalW += min._w;
-         }
-         else 
-         {
-           //std::cout << _vertexs[min._srci] << "->" <<_vertexs[min._dsti] << ":" << min._w << std::endl;
-           std::cout << "成环了"<< std::endl;
-         }
+        Edge min = minq.top();
+        minq.pop();
+        if (ufs.InSet(min._srci, min._dsti) == false)
+        {
+          // std::cout << _vertexs[min._srci] << "->" <<_vertexs[min._dsti] << ":" << min._w << std::endl;
+          minTree._AddEdge(min._srci, min._dsti, min._w);
+          ufs.Union(min._srci, min._dsti);
+          ++size;
+          totalW += min._w;
+        }
+        else
+        {
+          // std::cout << _vertexs[min._srci] << "->" <<_vertexs[min._dsti] << ":" << min._w << std::endl;
+          std::cout << "成环了" << std::endl;
+        }
       }
-      if(size != n-1)
+      if (size != n - 1)
       {
         return W();
       }
       return totalW;
+    }
+    // 最小生成树Prim 算法
+    W Prim(Self &minTree, const V &src)
+    {
+      // 最小生成树
+      int n = _vertexs.size();
+      minTree._vertexs = _vertexs;
+      minTree._indexMap = _indexMap;
+      minTree._matrix.resize(n, std::vector<W>(n, MAX_W));
+
+      size_t srci = GetVertexIndex(src);
+
+      std::set<size_t> X;
+      std::set<size_t> Y;
+      X.insert(srci);
+      for (size_t i = 0; i < n; i++)
+      {
+        if (i != srci)
+          Y.insert(i);
+      }
+
+      // 开始选边
+      std::priority_queue<Edge, std::vector<Edge>, std::greater<Edge>> minq;
+      for(size_t i = 0; i < n; i++)
+      {
+        if(_matrix[srci][i] != MAX_W)
+        {
+          minq.push(Edge(srci, i,_matrix[srci][i] ));
+        }
+      }
     }
 
     size_t GetVertexIndex(const V &v)
@@ -169,21 +200,21 @@ namespace matrix
         std::cout << i << " -> [ " << _vertexs[i] << " ]" << std::endl;
       }
       std::cout << std::endl;
-    
-      std::cout << "  "; 
+
+      std::cout << "  ";
       for (size_t i = 0; i < n; ++i)
         printf("%4d", i);
       std::cout << std::endl;
       for (size_t i = 0; i < n; ++i)
       {
-        std::cout << i << " "; 
+        std::cout << i << " ";
         for (size_t j = 0; j < n; ++j)
         {
-          if(_matrix[i][j] == MAX_W)
+          if (_matrix[i][j] == MAX_W)
           {
             printf("%4c", '*');
           }
-          else 
+          else
           {
             printf("%4d", _matrix[i][j]);
           }
@@ -200,18 +231,19 @@ namespace matrix
       if (Direction == false)
         _matrix[dsti][srci] = w;
     }
-    void _DFS(size_t root, std::vector<bool>& visited)
+    void _DFS(size_t root, std::vector<bool> &visited)
     {
       std::cout << root << " : " << _vertexs[root] << std::endl;
       visited[root] = true;
-      for(size_t i = 0; i < _matrix[root].size(); ++i)
+      for (size_t i = 0; i < _matrix[root].size(); ++i)
       {
-        if(_matrix[root][i] != MAX_W && visited[i] == false)
+        if (_matrix[root][i] != MAX_W && visited[i] == false)
         {
           _DFS(i, visited);
         }
       }
     }
+
   private:
     std::vector<V> _vertexs;
     std::map<V, int> _indexMap;
@@ -219,11 +251,11 @@ namespace matrix
   };
   void TestGraphMinTree()
   {
-    const char* str = "abcdefghi";
+    const char *str = "abcdefghi";
     Graph<char, int> g(str, strlen(str));
     g.AddEdge('a', 'b', 4);
     g.AddEdge('a', 'h', 8);
-    //g.AddEdge('a', 'h', 9);
+    // g.AddEdge('a', 'h', 9);
     g.AddEdge('b', 'c', 8);
     g.AddEdge('b', 'h', 11);
     g.AddEdge('c', 'i', 2);
@@ -236,15 +268,15 @@ namespace matrix
     g.AddEdge('g', 'h', 1);
     g.AddEdge('g', 'i', 6);
     g.AddEdge('h', 'i', 7);
-    
+
     Graph<char, int> kminTree;
     std::cout << "Kruskal:" << std::endl;
     g.Kruskal(kminTree);
     kminTree.Print();
-    //Graph<char, int> pminTree;
-    //std::cout << "Prim:" << g.Prim(pminTree, 'a') << std::endl;
-    //pminTree.Print();
-}
+    // Graph<char, int> pminTree;
+    // std::cout << "Prim:" << g.Prim(pminTree, 'a') << std::endl;
+    // pminTree.Print();
+  }
 
   void TestGraph()
   {
@@ -261,118 +293,113 @@ namespace matrix
   }
   void TestGraphDBFS()
   {
-    std::string a[] = { "张三", "李四", "王五", "赵六", "周七" };
-    Graph<std::string, int> g1(a, sizeof(a)/sizeof(std::string));
-    
+    std::string a[] = {"张三", "李四", "王五", "赵六", "周七"};
+    Graph<std::string, int> g1(a, sizeof(a) / sizeof(std::string));
+
     g1.AddEdge("张三", "李四", 100);
     g1.AddEdge("张三", "王五", 200);
     g1.AddEdge("王五", "赵六", 30);
     g1.AddEdge("王五", "周七", 30);
-    
-    //g1.Print();
-    //g1.BFS("张三");
+
+    // g1.Print();
+    // g1.BFS("张三");
     g1.DFS("张三");
   }
 }
 
-
 namespace link_table
 {
-    template <class W>
-    struct Edge
+  template <class W>
+  struct Edge
+  {
+    // int srci;
+    int _dsti; // 目标点的下标
+    W _w;      // 权值
+    struct Edge *_next;
+    Edge(int dsti, const W &w)
+        : _dsti(dsti), _w(w), _next(nullptr)
     {
-        //int srci;
-        int _dsti; // 目标点的下标 
-        W _w;     // 权值
-        struct Edge* _next;
-        Edge(int dsti, const W& w)
-            :_dsti(dsti)
-            , _w(w)
-            , _next(nullptr)
-        {
-
-        }
-
-    };
-
-    template <class V, class W, bool Direction = false>
-    class Graph
-    {
-    public:
-        typedef struct Edge<W> Edge;
-        Graph(const V* a, size_t n)
-        {
-            _vertexs.reserve(n);
-            for (size_t i = 0; i < n; ++i)
-            {
-                _vertexs.push_back(a[i]);
-                _indexMap[a[i]] = i;
-            }
-            _table.resize(n, nullptr);
-        }
-
-    public:
-        size_t GetVertexIndex(const V& v)
-        {
-            auto iter = _indexMap.find(v);
-            if (iter == _indexMap.end())
-            {
-                throw std::invalid_argument("顶点不存在");
-                return -1;
-            }
-            return iter->second;
-        }
-        // 添加边
-        void AddEdge(const V& src, const V& dst, const W& w)
-        {
-            size_t srci = GetVertexIndex(src);
-            size_t dsti = GetVertexIndex(dst);
-
-            Edge* eg = new Edge(dsti, w);
-            eg->_next = _table[srci];
-            _table[srci] = eg;
-
-            if (Direction == false)
-            {
-                Edge* eg2 = new Edge(srci, w);
-                eg2->_next = _table[dsti];
-                _table[dsti] = eg2;
-            }
-        }
-        void Print()
-        {
-            size_t n = _vertexs.size();
-            for (size_t i = 0; i < n; ++i)
-            {
-                std::cout << i << " -> [ " << _vertexs[i] << " ]" << std::endl;
-            }
-            std::cout << std::endl;
-            for (size_t i = 0; i < _table.size(); ++i)
-            {
-                std::cout << _vertexs[i] << "[ " << i << " ]-> ";
-                Edge* cur = _table[i];
-                while (cur)
-                {
-                    std::cout << "[ " <<_vertexs[cur->_dsti] <<":" << cur->_dsti << " ] -> ";
-                    cur = cur->_next;
-                }
-                std::cout << "nullptr" << std::endl;;
-            }
-           
-        }
-
-    private:
-        std::vector<V> _vertexs;
-        std::map<V, int> _indexMap;
-        std::vector<Edge*> _table;
-    };
-    void TestGraph()
-    {
-        std::string a[] = { "张三", "李四", "王五", "赵六" };
-        Graph< std::string, int, true> g1(a, 4);
-        g1.AddEdge("张三", "李四", 100);
-        g1.AddEdge("张三", "王五", 200);
-        g1.AddEdge("王五", "赵六", 30);
-        g1.Print();
     }
+  };
+
+  template <class V, class W, bool Direction = false>
+  class Graph
+  {
+  public:
+    typedef struct Edge<W> Edge;
+    Graph(const V *a, size_t n)
+    {
+      _vertexs.reserve(n);
+      for (size_t i = 0; i < n; ++i)
+      {
+        _vertexs.push_back(a[i]);
+        _indexMap[a[i]] = i;
+      }
+      _table.resize(n, nullptr);
+    }
+
+  public:
+    size_t GetVertexIndex(const V &v)
+    {
+      auto iter = _indexMap.find(v);
+      if (iter == _indexMap.end())
+      {
+        throw std::invalid_argument("顶点不存在");
+        return -1;
+      }
+      return iter->second;
+    }
+    // 添加边
+    void AddEdge(const V &src, const V &dst, const W &w)
+    {
+      size_t srci = GetVertexIndex(src);
+      size_t dsti = GetVertexIndex(dst);
+
+      Edge *eg = new Edge(dsti, w);
+      eg->_next = _table[srci];
+      _table[srci] = eg;
+
+      if (Direction == false)
+      {
+        Edge *eg2 = new Edge(srci, w);
+        eg2->_next = _table[dsti];
+        _table[dsti] = eg2;
+      }
+    }
+    void Print()
+    {
+      size_t n = _vertexs.size();
+      for (size_t i = 0; i < n; ++i)
+      {
+        std::cout << i << " -> [ " << _vertexs[i] << " ]" << std::endl;
+      }
+      std::cout << std::endl;
+      for (size_t i = 0; i < _table.size(); ++i)
+      {
+        std::cout << _vertexs[i] << "[ " << i << " ]-> ";
+        Edge *cur = _table[i];
+        while (cur)
+        {
+          std::cout << "[ " << _vertexs[cur->_dsti] << ":" << cur->_dsti << " ] -> ";
+          cur = cur->_next;
+        }
+        std::cout << "nullptr" << std::endl;
+        ;
+      }
+    }
+
+  private:
+    std::vector<V> _vertexs;
+    std::map<V, int> _indexMap;
+    std::vector<Edge *> _table;
+  };
+  void TestGraph()
+  {
+    std::string a[] = {"张三", "李四", "王五", "赵六"};
+    Graph<std::string, int, true> g1(a, 4);
+    g1.AddEdge("张三", "李四", 100);
+    g1.AddEdge("张三", "王五", 200);
+    g1.AddEdge("王五", "赵六", 30);
+    g1.Print();
+  }
 }

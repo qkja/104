74e265e720115f0471ec9069008a02f5bbb9b5e0
qkja <qkja1205@163.com>
2023-11-03 18:27:04 +0800

    提交

--- DataStructure/09 图/main.cpp
+++ DataStructure/09 图/main.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
-//#include "Graph.h"
-//int main()
-//{
-//	//matrix::TestGraph();
-//	//link_table::TestGraph();
-//	//matrix::TestGraphDBFS();
-//	//matrix::TestGraphMinTree();
-//	std::cout << "ÄãºÃ" << std::endl;
-//	return 0;
-//}
+#include "Graph.h"
+int main()
+{
+	//matrix::TestGraph();
+	//link_table::TestGraph();
+	//matrix::TestGraphDBFS();
+	//matrix::TestGraphMinTree();
+	//std::cout << "ÄãºÃ" << std::endl;
+	matrix::TestGraphMinTree();
+	return 0;
+}

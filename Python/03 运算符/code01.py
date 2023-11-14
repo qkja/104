36eaa85727496411db7e05bdf9833aca06253050
qkja <qkj1205@163.com>
2023-11-14 14:52:58 +0800

    运算符

---
+++ Python/03 运算符/code01.py
@@ -0,0 +1,51 @@
+# 先算乘方, 然后乘除,最后是加减 ,如果想要修改默认顺序,那么填上括号
+
+# result = 1 + 2 * 3
+# print(result)
+
+# print(10 / 0)
+
+
+# print(4**2)
+# print(4**3)
+# print(4**0.5)
+
+# print(7//2)
+# print(-7//2)
+
+
+# 关系运算符   -- 比较两个运算符的相对关系
+
+# a = 10
+# b = 20
+#
+# print(a < b)
+
+# a = "aaa"
+# b = "bbdd"
+#
+# # print(a < b)
+# print(a == b)
+# a = 1 / 3
+# b = 1 - 2 / 3
+# print(a == b)
+
+# 逻辑运算符
+
+
+# 赋值运算符
+
+
+
+# = 表示赋值,表示将右侧的值放在做的空间中
+
+# a , b = 1, 2
+# print(a)
+# print(b)
+
+# a = 10
+# b = 20
+# a , b = b, a
+# print(a)
+# print(b)
+print(True == 1)

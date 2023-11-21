77113e68e9f816e6c07df1b7c056aa40cb5d67f6
qkja <qkj1205@163.com>
2023-11-21 13:04:38 +0800

    字符串

---
+++ Python/10 代码样例/code03.py
@@ -0,0 +1,30 @@
+"""
+字符串相关
+1.单词逆序
+2.旋转字符串
+
+"""
+# def reverWords(s: str):
+#     tokens = s.split(' ')
+#     tokens.reverse()
+#     return ' '.join(tokens)
+#
+# str = "i am student."
+# print(reverWords(str))
+
+
+# def rotateStr(s, goal):
+#     if len(s) != len(goal):
+#         return False
+#     return goal in (s + s)
+
+# def conutPre(words: list, s: str):
+#     count = 0
+#     for word in words:
+#         if s.startswith(word):
+#             count += 1
+#     return count
+
+
+
+

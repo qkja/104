d34c04c2f786335d828f6947f3ea510c87b049f6
qkja <qkj1205@163.com>
2023-11-21 11:38:45 +0800

    日期只差

---
+++ Python/10 代码样例/code01.py
@@ -0,0 +1,6 @@
+"""
+标准库的使用
+"""
+
+# 标准库
+# https://docs.python.org/3.10/library/index.html
---
+++ Python/10 代码样例/code02.py
@@ -0,0 +1,16 @@
+"""
+日期计算工具
+功能: 计算两个时间相差的天数
+"""
+# import datetime
+
+# 1. 根据日期构造出 datetime变量
+# 2. 两个变量向减
+# date1 = datetime.datetime(2001, 11, 17)
+# date2 = datetime.datetime(2023, 11, 21)
+# print(date2 - date1)
+
+# from datetime import datetime
+# date1 = datetime(2001, 11, 17)
+# date2 = datetime(2023, 11, 21)
+# print(date2 - date1)

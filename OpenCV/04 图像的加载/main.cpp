0c1fb3a81983c5fa8b4bfe4b01350ca136b3532e
qkj <qkj1205@163.com>
2023-12-19 16:40:42 +0800

    提交

--- OpenCV/04 图像的加载/main.cpp
+++ OpenCV/04 图像的加载/main.cpp
@@ -53,36 +53,148 @@
 
 
 
+//#include <opencv2/opencv.hpp>
+//
+//int main()
+//{
+//	cv::VideoCapture video;
+//	video.open("./box.mp4");
+//	if (video.isOpened() == false)
+//	{
+//		std::cerr << "path error" << std::endl;
+//		return -1;
+//	}
+//
+//	std::cout << "帧率: " << video.get(cv::CAP_PROP_FPS) << std::endl;
+//	std::cout << "宽度: " << video.get(cv::CAP_PROP_FRAME_WIDTH) << std::endl;
+//	std::cout << "高度: " << video.get(cv::CAP_PROP_FRAME_HEIGHT) << std::endl;
+//
+//
+//	while (true)
+//	{
+//		cv::Mat frame;
+//		video >> frame;
+//		if (frame.empty())
+//			break;
+//		cv::imshow("video", frame);
+//		//阻塞等待一会
+//		uchar c = cv::waitKey(100 / video.get(cv::CAP_PROP_FPS));
+//		if (c == 'q')
+//		{
+//			break;
+//		}
+//	}
+//	return 0;
+//}
+
+//#include <opencv2/opencv.hpp>
+//
+//int main()
+//{
+//	cv::VideoCapture video;
+//	video.open("box.mp4");
+//	if (video.isOpened() == false)
+//	{
+//		std::cerr << "path error" << std::endl;
+//		return -1;
+//	}
+//
+//	std::cout << "帧率: " << video.get(cv::CAP_PROP_FPS) << std::endl;
+//	std::cout << "宽度: " << video.get(cv::CAP_PROP_FRAME_WIDTH) << std::endl;
+//	std::cout << "高度: " << video.get(cv::CAP_PROP_FRAME_HEIGHT) << std::endl;
+//
+//
+//	while (true)
+//	{
+//		cv::Mat frame;
+//		video >> frame;
+//		if (frame.empty())
+//			break;
+//		cv::imshow("video", frame);
+//		//阻塞等待一会
+//		uchar c = cv::waitKey(100 / video.get(cv::CAP_PROP_FPS) -3);
+//
+//
+//
+//		// 可以 通过视频的通道数来判断视频是灰色还是彩色的的
+//		bool isColor = frame.type() == CV_8UC3;
+//
+//		cv::VideoWriter writer;
+//		// 这是视频的编码
+//		int codec = cv::VideoWriter::fourcc('M', 'J', 'P', 'G');
+//		// 设置帧率
+//		double fps = 25.0;
+//		// 名称
+//		std::string filename = "live.avi";
+//		writer.open(filename, codec, fps, frame.size(), isColor);
+//		if (writer.isOpened() == false)
+//		{
+//			std::cerr << "path error" << std::endl;
+//			return -1;
+//		}
+//		if (c == 'q')
+//		{
+//			break;
+//		}
+//	}
+//	return 0;
+//}
+
+
 #include <opencv2/opencv.hpp>
 
 int main()
 {
+	cv::Mat frame;
 	cv::VideoCapture video;
-	video.open("./box.mp4");
+	video.open("box.mp4");
 	if (video.isOpened() == false)
 	{
 		std::cerr << "path error" << std::endl;
 		return -1;
 	}
 
-	std::cout << "帧率: " << video.get(cv::CAP_PROP_FPS) << std::endl;
-	std::cout << "宽度: " << video.get(cv::CAP_PROP_FRAME_WIDTH) << std::endl;
-	std::cout << "高度: " << video.get(cv::CAP_PROP_FRAME_HEIGHT) << std::endl;
+	video >> frame;
+	if (frame.empty())
+	{
+		std::cerr << "视频错误" << std::endl;
+		return -1;
+	}
+	// 可以 通过视频的通道数来判断视频是灰色还是彩色的的
+	bool isColor = frame.type() == CV_8UC3;
+
+	cv::VideoWriter writer;
+	// 这是视频的编码
+	int codec = cv::VideoWriter::fourcc('M', 'J', 'P', 'G');
+	// 设置帧率
+	double fps = 25.0;
+	// 名称
+	std::string filename = "live.avi";
+	writer.open(filename, codec, fps, frame.size(), isColor);
+	if (writer.isOpened() == false)
+	{
+		std::cerr << "path error" << std::endl;
+		return -1;
+	}
 
 
 	while (true)
 	{
-		cv::Mat frame;
-		video >> frame;
 		if (frame.empty())
-			break;
-		cv::imshow("video", frame);
-		//阻塞等待一会
-		uchar c = cv::waitKey(100 / video.get(cv::CAP_PROP_FPS));
-		if (c == 'q')
 		{
 			break;
 		}
+
+		// 写进入 视频
+		writer.write(frame);
+		cv::imshow("img", frame);
+		video >> frame;
+		uchar c = cv::waitKey(50);
+	    if (c == 'q')
+			break;
+
 	}
+
+
 	return 0;
 }
---
+++ OpenCV/05 图像/test.cpp
@@ -0,0 +1,57 @@
+#include <opencv2/opencv.hpp>
+#include <string>
+#include <vector>
+#include <iostream>
+using namespace std;
+using namespace cv;
+
+//int main()
+//{
+//	string path = "1.png";
+//	Mat img = imread(path);
+//	Mat img32;
+//	img.convertTo(img32, CV_32F, 1 / 255.0, 0);
+//	/*Mat HSV;
+//	Mat HSV32;*/
+//	
+//	/*cvtColor(img, HSV, COLOR_BGR2HSV);
+//	cvtColor(img32, HSV32, COLOR_BGR2HSV);*/
+//
+//	Mat gray0, gray1;
+//	cvtColor(img, gray0, COLOR_BGR2GRAY);
+//	cvtColor(img32, gray1, COLOR_RGB2GRAY);
+//
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	string path = "1.png";
+//	Mat img = imread(path);
+//
+//	Mat imgs[3];
+//	// ∑÷¿Î
+//	split(img, imgs);
+//	Mat img1 = imgs[0];
+//	Mat img2 = imgs[1];
+//	Mat img3 = imgs[2];
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	string path = "1.png";
+//	Mat img = imread(path);
+//
+//	Mat imgs[3];
+//	// ∫œ≤¢
+//	split(img, imgs);
+//	Mat img_copy;
+//	merge(imgs, 3, img_copy);
+//
+//	return 0;
+//}
+
+
